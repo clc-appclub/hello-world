7d3929f32c000d0613a070ea9563224167fd412e
trindonut <trinitydonohugh@gmail.com>
2017-03-16 13:08:00 +0000

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -3,9 +3,16 @@ name = input("what is your name?")
 print ("hello", name)
 
 number = int(input("How old are you?"))
-print ("Hello, you are ", number, "years old")
-           
+print ("Hello, you are ", number, "years old")       
 
+control = input("Do you want to perform an addition calculation - True or False?")
+
+if control == True:
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number"))
+    ans = num1 + num2
+    print ("Your answer is: ", ans)
+else, 
           
 
              
