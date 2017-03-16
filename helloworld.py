30685a2d8f6f9b6833541e687303b212fc330bf0
trindonut <trinitydonohugh@gmail.com>
2017-03-16 12:36:20 +0000

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -2,6 +2,9 @@ print ("Hello, world")
 name = input("what is your name?")
 print ("hello", name)
 
+number = int(input("How old are you?"))
+print ("Hello, you are ", number, "years old")
+           
 
           
 
